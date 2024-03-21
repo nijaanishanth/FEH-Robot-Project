d13879ab38a8f0c7bff2bcebe6151ebab7d19f47
Nijaa Nishanth <83176724+nijaanishanth@users.noreply.github.com>
2024-03-21 19:20:02 -0400

    Update Checkpoint3.cpp

--- Checkpoint3.cpp
+++ Checkpoint3.cpp
@@ -26,10 +26,10 @@
 #define turn_speed 25
 
 // turns
-#define left90 240.0
-#define left45 120.0
-#define right90 295.0
-#define right45 147.5
+#define left90 200.0
+#define left45 92.5
+#define right90 200.0
+#define right45 100.0
 
 // declare motors & cds cell
 FEHMotor right_motor(FEHMotor :: Motor0, 9.0);
@@ -37,7 +37,7 @@ FEHMotor left_motor(FEHMotor :: Motor2, 9.0);
 FEHMotor vex_motor(FEHMotor :: Motor3, 9.0);
 AnalogInputPin Cds_cell(FEHIO :: P1_0);
 DigitalEncoder right_encoder(FEHIO :: P0_0);
- DigitalEncoder left_encoder(FEHIO :: P3_0);
+DigitalEncoder left_encoder(FEHIO :: P3_0);
 
 //Encoder has 318 counts per revolution
 //wheel radius and pi can be constants
@@ -46,8 +46,8 @@ void goStraight(float inches, int percent){
     left_encoder.ResetCounts();
     float num_revolutions = inches/(circ);
     while(right_encoder.Counts() < 318 * num_revolutions){
-        right_motor.SetPercent(-percent);
-        left_motor.SetPercent(percent);
+        right_motor.SetPercent(percent);
+        left_motor.SetPercent(-percent);
     }
     right_motor.Stop();
     left_motor.Stop();
@@ -70,8 +70,8 @@ void turnRight(float counts, int percent_right, int percent_left){
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
     while(right_encoder.Counts() < counts){
-        right_motor.SetPercent(-percent_right);
-        left_motor.SetPercent(-percent_left);
+        right_motor.SetPercent(percent_right);
+        left_motor.SetPercent(percent_left);
     }
     right_motor.Stop();
     left_motor.Stop();
@@ -82,8 +82,8 @@ void turnLeft(float counts, int percent_right, int percent_left){
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
     while(right_encoder.Counts() < counts){
-        right_motor.SetPercent(percent_right);
-        left_motor.SetPercent(percent_left);
+        right_motor.SetPercent(-percent_right);
+        left_motor.SetPercent(-percent_left);
     }
     right_motor.Stop();
     left_motor.Stop();
@@ -108,6 +108,7 @@ int checkLight(AnalogInputPin Cds_cell){
 }
 
 // rack and pinion go up function
+/*2 seconds = 1.5 inches*/
 void goUp(float time)
 {
     float start_time = TimeNow();
@@ -115,16 +116,22 @@ void goUp(float time)
     {
         vex_motor.SetPercent(25);
     }
+    vex_motor.Stop();
 }
 
 // rack and pinion go down function
-void goDown(float time)
+void goDown(float inches)
 {
+    // 1 SECOND = 0.95 INCHES
+    // MAX INCHES IS 7.5
+    float time = inches/0.95;
     float start_time = TimeNow();
     while(TimeNow() - start_time < time)
     {
         vex_motor.SetPercent(-25);
     }
+        vex_motor.Stop();
+
 }
 
 /* Main function */
@@ -134,36 +141,59 @@ int main(void)
     // CHECKPOINT 3
     //initialize the RCS
     // team key: D30D1Hj1u
-    /*RCS.InitializeTouchMenu("D30D1Hj1u");
 
+    // while loop to sleep until cds cell is on
+    while(Cds_cell.Value() >= 1.0){
+        Sleep(0.05);
+    }
+
+    // RCS.InitializeTouchMenu("D30D1Hj1u");
+
+    int lever_distance = 0;
     // Check which lever to flip and perform some action
-    int correctLever = RCS.GetCorrectLever();
+    // int correctLever = RCS.GetCorrectLever();
+    int correctLever = 1;
     if(correctLever == 0)
     {
-        // Perform actions to flip left lever
+        lever_distance = 3.0;
     } 
     else if(correctLever == 1)
     {
-        // Perform actions to flip middle lever
+        lever_distance = 6.8;
     }
     else if(correctLever == 2)
     {
-       // Perform actions to flip right lever
-    }*/
+        lever_distance = 10.4;
+    }
 
-    goDown(1.0);
-    
     // start hook at the top of rack
+    // go straight 11
+    goStraight(14.25, 25);
 
     // turn left
+    turnLeft(left45, 25, 25);
 
     // go straight till fuel levers
+    goStraight(lever_distance, 25);
 
     // move hook down
+    goDown(3.75);
 
-    // wait 5 seconds
+    // backwards
+    goBackward(lever_distance, 25);
+    Sleep(5.0);
+
+    // move down a little
+    goDown(0.75);
+
+    // forwards again
+    goStraight(lever_distance, 25);
 
     // move hook back up
+    goUp(1.75);
 
+    // go backwards and reset
+    goBackward(lever_distance, 25);
+    goUp(3.5);
 
 }
